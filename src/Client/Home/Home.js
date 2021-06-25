19f879e0295715820b16e576525e357344758bd5
Mubasheer <Mdmubasheer6544@gmail.com>
2021-06-24 19:35:45 -0700

    version 1.5

--- src/Client/Home/Home.js
+++ src/Client/Home/Home.js
@@ -1,36 +1,34 @@
 import React from "react";
 import Categories from "../Categories/Categories";
 import Haeder from "../Header/Header";
-import { Switch, Route,Redirect } from "react-router-dom";
+import { Switch, HashRouter, Route, Redirect } from "react-router-dom";
 import MovieDetails from "../MoviesDetails/MovieDetails";
 import Footer from "../Footer/Footer";
 import Search from "../Search/Search";
 import FirstPage from "./FirstPage";
 import Login from "../Login/Login";
 
-
-
-const Master=()=>{
-    return(<Switch>
-        <Route exact path="/">
-         <Redirect to="/login"/>
-        </Route>
+const Master = () => {
+  return (
+    <Switch>
+      <HashRouter basename="/login">
         <Route exact path="/login" component={Login} />
-        <Route component={Home}/>
-    </Switch>)
-}
+        <Route component={Home} />
+      </HashRouter>
+    </Switch>
+  );
+};
 const Home = () => {
   return (
     <div>
       <Haeder />
-     
-        <Route exact path="/home" component={FirstPage} />
-        <Route exact path="/show_movie_details/:id" component={MovieDetails} />
-        <Route exact path="/search/:key" component={Search} />
-        <Route exact path="/categorie/:id" component={Categories} />
+      <Route exact path="/home" component={FirstPage} />
+      <Route exact path="/show_movie_details/:id" component={MovieDetails} />
+      <Route exact path="/search/:key" component={Search} />
+      <Route exact path="/categorie/:id" component={Categories} />
       <Footer />
     </div>
   );
 };
 
-export default Master ;
+export default Master;
